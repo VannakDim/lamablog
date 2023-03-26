7adfed022a2fe1048e84453e8213daf3b10e75dc
VannakDim <hannah@Vannaks-MacBook-Pro.local>
2023-03-26 20:45:46 +0700

    uncomment token

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -19,7 +19,7 @@ export const getPosts = (req, res) => {
 
 export const getPost = (req, res) => {
   const q =
-    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+    "SELECT p.id, `username`, `title`, `description`, p.img, u.img AS userImg, `cat`,`postDate` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -29,11 +29,11 @@ export const getPost = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-  // const token = req.cookies.access_token;
-  // if (!token) return res.status(401).json("Not authenticated!");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not authenticated!");
 
-  // jwt.verify(token, "jwtkey", (err, userInfo) => {
-  //   if (err) return res.status(403).json("Token is not valid!");
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
 
     const q =
       "INSERT INTO posts(`title`, `description`, `cat`, `postDate`,`uid`) VALUES (?)";
@@ -52,7 +52,7 @@ export const addPost = (req, res) => {
       if (err) return res.status(500).json(err);
       return res.json("Post has been created.");
     });
-//   });
+  });
 };
 
 export const deletePost = (req, res) => {
